5740cfe538517b361a83ccc929f55e408888aee6
AshcroftGamer <lippeativista@gmail.com>
2021-07-08 16:38:48 -0300

    Arquivos modificados

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const bodyparser = require('body-parser')
 app.use(morgan('dev'))
 
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
 
 
 
@@ -12,19 +14,58 @@ const rotaFunciorario = require('./route/funcionario');
 const rotaProprietario = require('./route/proprietario');
 
 
+
+app.use(express.static('public'));
+
+
+
 app.use('/cardapio', rotaCardapio)
 app.use('/funcionario', rotaFunciorario)
 app.use('/proprietario', rotaProprietario)
-app.use(bodyparser.urlencoded({extended: false}));
-app.use(bodyparser.json());
-app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
 
+// ? CORS - CROSS ORIGIN RESOUCE SHARING
+// * Conjunto de regras nas quais determina o que pode ser acessado e o que pode ...
+// * não pode ser, com limites de acesso, entre outros.
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin,X-Requested-With,Content-Type,Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).send({});
+    }
+    next();
+});
+
+// ! Tratamento criado para quando não for encontrado nenhuma rota
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+
+    return res.send({
+        erro: {
+            message: error.message
+        }
+    });
+});
+
+
+
 app.use('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
+
 
 module.exports = app;
--- route/proprietario.js
+++ route/proprietario.js
@@ -1,33 +1,188 @@
-const express = require('express');
+const express = require( 'express' );
 const prop = express.Router();
+const bd = require( '../mysql' ).pool;
+
+
+prop.get( '/', ( req, res ) => {
+    bd.getConnection( ( err, conn ) => {
+        if ( err ) {
+            return res.status( 500 ).send( { erro: err } )
+        }
+        conn.query( 'SELECT * FROM proprietario;',
+            ( err, result, fields ) => {
+                if ( err ) {
+                    return res.status( 500 ).send( { erro: err } )
+                }
+                const response = {
+                    proprietario: result.map( data => {
+                        return {
+                            id_proprietario: data.id_proprietario,
+                            nome: data.nome,
+                            email: data.email,
+                            telefone: data.telefone,
+                            senha: data.senha
+
+                        }
+                    } )
+
+
+                }
+                return res.status( 200 ).send( response );
+            }
+        )
+    } )
+} )
 
 
 
-prop.get('/', (req, res) => {
-    res.send("Bem vindo a rota proprietario")
-    
-})
+
+
+prop.post( '/cadastro', ( req, res ) => {
+    console.log( req.body )
+    bd.getConnection( ( err, conn ) => {
+        if ( err ) {
+            return res.status( 500 ).send( { erro: err } )
+        }
+        conn.query( 'INSERT INTO proprietario (nome, email, telefone, senha ) VALUES (?, ?, ?, ?)',
+
+            [
+                req.body.nome,
+                req.body.email,
+                req.body.telefone,
+                req.body.senha
+            ],
+
+
+            ( err, result ) => {
+
+                conn.release();
+                if ( err ) {
+                    return res.status( 500 ).send( { erro: err } )
+                }
+                const response = {
+                    mensagem: 'Conta criada com sucesso!',
+
+                }
+                return res.status( 200 ).send( response );
+            }
+        )
+    } )
+} )
 
 
 
-prop.post('/', (req, res) => {
-    res.send("Bem vindo a rota proprietario")
-    
-})
+
+
+prop.patch( '/update/:id', ( req, res ) => {
+    // let id = req.params.id;
+    bd.getConnection( ( err, conn ) => {
+        if ( err ) {
+            return res.status( 500 ).send( {
+                error: err
+            } )
+        }
+        conn.query( `UPDATE proprietario
+                    SET nome                  = ?,
+                        telefone              = ?,
+                        senha                 = ?
+                        where id_proprietario = ?`,
+            [
+                req.body.nome,
+                req.body.telefone,
+                req.body.senha,
+                req.params.id
+            ],
+            ( err, result ) => {
+
+                conn.release();
+                console.log( 'entrous' )
+                console.log( err )
+                console.log( 'saiu' )
+
+                if ( err ) {
+                    res.status( 500 ).send( {
+                        erro: err
+                    } )
+                }
+                else {
+
+                    const response = {
+                        mensagem: 'Dados alterados com sucesso!',
+                    };
+
+                    return res.status( 202 ).send( response );
+                }
+
+            } )
+
+    } )
+
+} )
+
+prop.put( '/update/:ide', ( req, res ) => {
+    bd.getConnection( ( error, conn ) => {
+        if ( error ) {
+            return res.status( 500 ).send( {
+                erro: error
+            } )
+        }
+        conn.query( 'UPDATE proprietario SET nome = ?, telefone = ?,senha  = ? where id_proprietario = ?',
+            [
+                req.body.nome,
+                req.body.telefone,
+                req.body.senha,
+                req.params.ide
+            ],
+            (error, results) => {
+                conn.release();
+
+                if(error){
+                    return res.status(500).send({
+                        erro: error
+                    })
+                }
+                const response = {
+                    mensagem: 'Usuario atualizado com sucesso!'
+                }
+
+                return res.status(500).send(response)
+            }
+        )
+    } )
+} )
 
 
 
-prop.patch('/', (req, res) => {
-    res.send("Bem vindo a rota proprietario")
-    
-})
+prop.delete( '/remove/:id', ( req, res ) => {
+    let id = req.params.id;
+    bd.getConnection( ( err, conn ) => {
+        if ( err ) {
+            res.status( 500 ).send( {
+                erro: err
+            } )
+        }
+        conn.query(
+            `DELETE FROM proprietario WHERE id_proprietario = ?`,
+            [
+                id
+            ],
+            ( err, result, field ) => {
+                conn.release();
+                if ( err ) {
+                    res.status( 500 ).send( {
+                        erro: err
+                    } )
+                }
+                const response = {
+                    mensagem: 'Usuario removido do sistema!',
+                }
 
+                return res.status( 500 ).send( response )
+            }
+        )
+    } )
 
-
-prop.delete('/', (req, res) => {
-    res.send("Bem vindo a rota proprietario")
-    
-})
+} );
 
 
 module.exports = prop;
