92a1f03e36e5d498c951706f5bc8e404a23f5f74
Felipe Goes <34778191+AshcroftGamer@users.noreply.github.com>
2021-07-13 17:31:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const express = require('express');
+const express = require( 'express' );
 const app = express();
-const morgan = require('morgan')
+const morgan = require( 'morgan' );
+const bodyparser = require( 'body-parser' );
+
+app.use( bodyparser.urlencoded( { extended: false } ) );
+app.use( bodyparser.json() );
+
+const rotaCardapio = require( './route/cardapio' );
+const rotaFunciorario = require( './route/funcionario' );
+const rotaProprietario = require( './route/proprietario' );
+
+app.use( '/cardapio', rotaCardapio );
+app.use( '/funcionario', rotaFunciorario );
+app.use( '/proprietario', rotaProprietario );
+
+app.use( morgan( 'dev' ) );
+app.use( express.static( 'public' ) );
+app.use( '/uploads', express.static( 'uploads' ) );
+app.use( '/css', express.static( __dirname + 'public/css' ) );
+app.use( '/js', express.static( __dirname + 'public/js' ) );
+app.use( '/img', express.static( __dirname + 'public/img' ) );
 
 
-app.use(express.static('public'));
-app.use('/uploads', express.static('uploads'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-app.use('/img', express.static(__dirname + 'public/img'));
-
-app.use(morgan('dev'))
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+app.get( '/', ( req, res ) => {
+    res.sendFile( __dirname + '/index.html' );
+} )
+app.get( '/dashboard', ( req, res ) => {
+    res.sendFile( __dirname + '/public/pages/dashboard.html' );
+} )
 
 app.get('/cadastro', (req, res) => {
     res.sendFile(__dirname + '/public/pages/cadastro.html');
@@ -21,5 +35,9 @@ app.get('/cadastro', (req, res) => {
 app.get('/home', (req, res) => {
     res.sendFile(__dirname + '/public/pages/home.html');
 })
+app.use( '*', ( req, res ) => {
+    res.sendFile (__dirname + '/public/pages/error.html' );
+} )
+
 
 module.exports = app;
