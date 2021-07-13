0559aed0740413710a0fc935e79a406116ffbd36
Felipe Goes <34778191+AshcroftGamer@users.noreply.github.com>
2021-07-13 17:21:46 -0300

    arquive update

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const morgan = require('morgan')
 
 
-
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
 app.use('/css', express.static(__dirname + 'public/css'));
