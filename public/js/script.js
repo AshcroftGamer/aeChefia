bba84faad7a084ef3b3cb82e57ad75268fff56fc
Juan Pablo <juam.euzin@gmail.com>
2021-08-05 16:56:58 -0300

    05/08/2021 termindano a splash screen e botao de ver senha

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+
 function ok() {
   alert( "clickou" )
 };
@@ -13,6 +14,39 @@ function process( mesa ) {
   }
 
 }
+// function rodaSenha(){
+
+//     let senha = document.getElementById('senha')
+//   // let olho = document.getElementById('olho')
+//   senha.style.backgroundImage.setAttribute = ("onclick", ViewSenha())
+// }
+
+
+function ViewSenha(){
+  let senha = document.getElementById('senha')
+// let olho = document.getElementById('olho')
+
+if(senha.getAttribute('type') == ("password") ){
+  senha.setAttribute("type", "text")
+  document.getElementById("olho").src = "/img/view.png"
+  console.log("rodou1")
+}else{
+    senha.setAttribute("type", "password")
+document.getElementById("olho").src = "/img/hidden.png"
+console.log("rodou2")
+}
+
+}
+
+const splash = document.querySelector('.splash');
+document.addEventListener('DOMContentLoaded',(e)=>{
+  setTimeout(() => {
+    splash.classList.add('display-none');
+  }, 2000);
+})
+
+
+
 // var check_menu = document.querySelector('#menu-js')
 // var menu1 = document.getElementById('menu')
 // function menu(){
@@ -189,7 +223,7 @@ async function cadastro() {
 // }
 
 
-//A variável de ID ficou fora das funções para manter o estado...
+// A variável de ID ficou fora das funções para manter o estado...
 let id = 0;
 
 //Retorna o próximo ID válido para a DIV que será criada
@@ -197,44 +231,6 @@ function getNextId() {
   return ++id;
 }
 
-//Sua função, chamada no click do botão
-<<<<<<< HEAD
-function criarDiv() {
-  //Pego o ID
-  let id = getNextId();
-  //Crio a DIV
-  let divElement = document.createElement( "div" );
-  var conteudoNovo = document.createTextNode( id );
-  divElement.appendChild( conteudoNovo );
-  //Pego a DIV onde a nova DIV será criada, sempre na DIV mãe
-  let divMae = document.getElementById( "mesas" );
-
-  //A ideia do ID é que ele seja um elemento único, ou seja, não se repita
-  divElement.setAttribute( 'id', 'box' + id.toString() );
-
-  //CSS
-  divElement.style.width = "66px";
-  divElement.style.height = "66px";
-  divElement.style.backgroundColor = '#666666';
-  divElement.style.display = '';
-  divElement.style.opacity = '0.1';
-  divElement.style.marginLeft = '5%';
-  divElement.style.margin = '10px'
-  divElement.classList.add( "bounceIn" )
-  divElement.style.paddingTop = '23px'
-  divElement.style.textAlign = 'center'
-
-
-
-  //Essa parte é mais para deixar claro que outras divs estão sendo criadas, criando um degrade
-  //divElement.style.backgroundColor = "#f0" + id.toString();
-
-  //Adiciono a nova DIV na DIV mãe
-  //Aqui poderia ser por exemplo document.body.appendChild, adicionando assim o elemento criado diretamente no body
-  divMae.appendChild( divElement );
-  // document.body.appendChild(divElement)
-  console.log( "rodou" )
-=======
 function criarDiv(){
     //Pego o ID
     let id = getNextId();
@@ -270,7 +266,6 @@ function criarDiv(){
     divMae.appendChild(divElement);
     // document.body.appendChild(divElement)
     console.log("rodou")
->>>>>>> bc2bb8e39242b0ea89fe28223701f0bd1ef3492c
 }
 
 function criarDivCardapio(){
@@ -316,7 +311,6 @@ function criarDivCardapio(){
   // document.body.appendChild(divElement)
   console.log("rodou")
 }
-
 
 
 
