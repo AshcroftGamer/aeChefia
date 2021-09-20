28c49926b789a02b34cd7ba3df1fbdb74e1ec7fb
Caetano Nascimento <caetanopark@gmail.com>
2021-09-20 17:04:24 -0300

    fetch cadastro proprietario feito

--- route/cadastro.js
+++ route/cadastro.js
@@ -17,33 +17,8 @@ const storage = multer.diskStorage( {
 } )
 const upload = multer( { storage } )
 
-function checkAuthenticated(req, res, next){
-    let token = req.cookies['session-token'];
 
-    let user = {};
-
-    async function verify() {
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: CLIENT_ID,
-        });
-        const payload = ticket.getPayload();
-        user.name = payload.name;
-        user.email = payload.email;
-        user.picture = payload.picture;
-    }
-    verify()
-    .then(()=>{
-        req.user = user;
-        next();
-    })
-    .catch(err=>{
-        res.redirect('/')
-    })
-
-}
-
-route.get('/', checkAuthenticated, (req, res) => {
+route.get('/',  (req, res) => {
     // res.send('ok')
     res.sendFile(__basedir + '/public/pages/cadastro.html');
 })
